794a5a577a99d079920cf0b9a75b9c955d7117c9
Nikkei96 <nikitarudentsov@yandex.ru>
2020-11-03 15:06:55 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id='app'>123</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
