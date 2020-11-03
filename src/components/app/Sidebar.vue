191f631589314a3426deba4ad8ee0109fd1ae59d
Nikkei96 <nikitarudentsov@yandex.ru>
2020-11-03 18:32:16 +0300

    splited mainlayot into components and added datetime filter with Intl

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class='sidenav app-sidenav' :class='{open: isOpen}'>
+    <router-link
+      v-for='(link, index) of links'
+      :key='index'
+      tag='li'
+      active-class='active'
+      :to='link.path'
+      :exact='link.exact'
+    >
+      <a class='waves-effect waves-orange pointer'>{{ link.name }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    isOpen: {
+      required: true,
+      type: Boolean,
+    },
+  },
+  data: () => ({
+    links: [
+      { name: 'Счёт', path: '/', exact: true },
+      { name: 'История', path: '/history' },
+      { name: 'Планирование', path: '/planning' },
+      { name: 'Новая запись', path: '/record' },
+      { name: 'Категории', path: '/categories' },
+    ],
+  }),
+}
+</script>
