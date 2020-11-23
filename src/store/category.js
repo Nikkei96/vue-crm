a7baed3a808e6ca28dc75b24fc798d811237f5c2
Nikkei96 <nikitarudentsov@yandex.ru>
2020-11-23 17:35:11 +0300

    added create and edit categories (l.11)

--- src/store/category.js
+++ src/store/category.js
@@ -11,6 +11,41 @@ export default {
         ctx.commit('setError', e)
         throw e
       }
+    },
+
+    async fetchCategories(ctx) {
+      try {
+        const uid = await ctx.dispatch('getUid')
+        const categories = (await firebase.database().ref(`/users/${uid}/categories`).once('value')).val() || {}
+
+        // const cats = []
+        // Object.keys(categories).forEach(key => {
+        //   cats.push({
+        //     title: categories[key].title,
+        //     limit: categories[key].limit,
+        //     id: key
+        //   })
+        // })
+        // return cats
+
+        return Object.keys(categories).map(key => ({
+          ...categories[key],
+          id: key,
+        }))
+      } catch (e) {
+        ctx.commit('setError', e)
+        throw e
+      }
+    },
+
+    async updateCategory(ctx, { id, title, limit }) {
+      try {
+        const uid = await ctx.dispatch('getUid')
+        await firebase.database().ref(`/users/${uid}/categories`).child(id).update({ title, limit })
+      } catch (e) {
+        ctx.commit('setError', e)
+        throw e
+      }
     }
   }
 }
--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,9 +4,16 @@
       <h3>Категории</h3>
     </div>
     <section>
-      <div class='row'>
+      <Loader v-if='loading' />
+      <div class='row' v-else>
         <CategoryCreate @created='addNewCategory' />
-        <CategoryEdit />
+        <CategoryEdit
+          v-if='categories.length'
+          :categories='categories'
+          @updated='updateCategories'
+          :key='categories.length + updateCount'
+        />
+        <p v-else class='center'>Категорий пока нет!</p>
       </div>
     </section>
   </div>
@@ -21,13 +28,26 @@ export default {
 
   data: () => ({
     categories: [],
+    loading: true,
+    updateCount: 0,
   }),
 
   methods: {
     addNewCategory(category) {
       this.categories.push(category)
-      console.log(this.categories)
     },
+
+    updateCategories(category) {
+      const idx = this.categories.findIndex(c => c.id === category.id)
+      this.categories[idx].title = category.title
+      this.categories[idx].limit = category.limit
+      this.updateCount++
+    },
+  },
+
+  async mounted() {
+    this.categories = await this.$store.dispatch('fetchCategories')
+    this.loading = false
   },
 }
 </script>
